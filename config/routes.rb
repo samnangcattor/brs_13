aee663f510451400e2234a537301fd397057c0e8
DamSamNang <kcattor@gmail.com>
2015-06-18 08:12:58 +0700

    Follow and unfollow user

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
   resources :users
+  get "users/:id/:status", to: "relationships#index", as: :users_with_status
   devise_for :admins,
     class_name: "User",
-    controllers: {sessions: "admins/sessions"},
+    controllers: {sessions: "admins/sessions", only: [:create]},
     only: :sessions
   namespace :admins do
     root "users#index"
-    devise_for :users
+    devise_for :users, only: [:create, :show, :destroy]
     resources :users
   end
+  resources :relationships, only: [:index, :create, :destroy]
 end
