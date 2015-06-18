3efda6d7ce43febbad2ca14f6e2afbad7081bd13
DamSamNang <kcattor@gmail.com>
2015-06-18 08:12:58 +0700

    Activity_read_favorite_comment_review_of_user

--- app/models/book_favorite.rb
+++ app/models/book_favorite.rb
@@ -1,4 +1,11 @@
 class BookFavorite < ActiveRecord::Base
+  after_create :activity_favorite
+
   belongs_to :user
   belongs_to :book
+
+  private
+  def activity_favorite
+    user.activities.create target_id: id, state_target: Settings.favorite
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,13 @@
 class Comment < ActiveRecord::Base
+  after_create :activity_comment
+
   belongs_to :user
   belongs_to :review
 
   validates :comment, presence: true, length: {maximum: 500}
+
+  private
+  def activity_comment
+    user.activities.create target_id: id, state_target: Settings.comment
+  end
 end
